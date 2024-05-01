3860180ea3d50885d8c35833b60bf11f0755ff3f
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2024-05-01 17:45:28 +0900

    Add files via upload

--- CWT_threshold.py
+++ CWT_threshold.py
@@ -36,7 +36,7 @@ def wavelet_convolution(tup):
 
 # CWT를 하되, main freq 주변 부위 제거하고 CWT돌리기!
 
-data_path ="C:\\Users\\SM-PC\\Desktop\\초음파_modified CWT\\초음파_modified CWT\\raw_data\\h_plain\\tp_240325_tek0061ALL.csv"
+data_path ="C:\\Users\\SM-PC\\Desktop\\초음파_modified CWT\\초음파_modified CWT\\raw_data\\r\\tp_240327_tek0002ALL.csv"
 data = pd.read_csv(data_path, skiprows=20)
 time = data['TIME']
 ch1 = data['CH1']
@@ -61,7 +61,7 @@ Fs = 1 / Ts
 freq = np.fft.fftfreq(n, d=Ts)[:n // 2]
 Y = np.fft.fft(ch1) / n
 Y = Y[:n // 2]
-Y = np.abs(Y)/np.max(Y)
+Y = np.abs(Y)/np.max(Y) #FFT normalization
 magnitude = abs(Y)
 
 # 피크 찾기
@@ -81,17 +81,62 @@ print(f"The third largest peak is at {th_peak_freq} Hz with a magnitude of {th_p
 
 # th_peak_magnitude 기준으로  *0.8이상의 신호가 들어온 곳의 time값 찾기
 th = th_peak_magnitude*0.8
-print(th)
+#print(th)
 
+# 4400, 5000 범위에서 peak mag값 찾아야 됨.......FFT측면에서 적용해보자
+
+focused_indices = np.where((freq>=44000) & (freq<=50000))
+focused_max = np.max(np.abs(Y[focused_indices]))
+#print(focused_max)
+
+# focused_max 기준으로 0.8이상의 신호가 들어온 th
+focused_th = focused_max*0.9
+#print(focused_th)
+
+focused_freq = freq[focused_indices]
+focused_Y = np.abs(Y[focused_indices])
+
+
+# focused 에서 진폭 정렬 및 세 번째로 큰 값 찾기
+f_peaks, f_properties = find_peaks(focused_Y, height=0)  # 모든 피크 찾기
+
+f_peak_magnitudes = f_properties['peak_heights']
+
+f_sorted_peaks = np.argsort(f_peak_magnitudes)[::-1]  # 진폭을 내림차순으로 정렬
+f_peak_index = f_sorted_peaks[2]  # 여섯번째로 큰 값의 인덱스
+
+f_th_peak_freq = freq[peaks[f_peak_index]]
+f_th_peak_magnitude = f_peak_magnitudes[f_peak_index]
+print(f"The third largest focused peak is at {f_th_peak_freq} Hz with a magnitude of {f_th_peak_magnitude}.")
+
+
+max_fY_index = np.where(focused_Y == np.max(focused_Y))
+
+#plt.plot(focused_freq, focused_Y)
+#plt.scatter(focused_freq[max_fY_index],focused_Y[max_fY_index], color='red')
+#plt.show()
+
+#print(np.max(focused_Y))
 # 결과 시각화
+
+plt.figure(figsize=(12, 5))
+plt.subplot(2,1,1)
 #plt.imshow(np.abs(cwt_result), extent=[t[0], t[-1], 20, n], aspect='auto',cmap='jet', vmin=0, vmax=20)
-plt.imshow(normal_cwt, extent=[time.iloc[0], time.iloc[-1], 20, n], aspect='auto',cmap = 'jet', vmin=th, vmax=1)
+#plt.imshow(normal_cwt, extent=[time.iloc[0], time.iloc[-1], 20, n], aspect='auto',cmap = 'jet', vmin=th, vmax=1)
+plt.imshow(normal_cwt, extent=[time.iloc[0], time.iloc[-1], 20, n], aspect='auto',cmap = 'jet', vmin=f_th_peak_magnitude*0.9, vmax=f_th_peak_magnitude)
+plt.ylim([4400,5000])
 plt.colorbar()
 plt.xlabel('Time')
 plt.ylabel('Scale')
-
+plt.ylim([4250, 5750]) # main 신호 전체
+plt.xlim([-0.00015, 0.0030])
 
 plt.title(f'Continuous Wavelet Transform(wavelet_width = {wavelet_length})')
 
+plt.subplot(2,1,2)
+plt.plot(freq, Y)
+plt.title("FFT")
+plt.xlim([42500,57500])
 plt.show()
+
 
